eb45355733349fa06599f041dee1a61254d85d85
SOM423 <75077641+SOM423@users.noreply.github.com>
2022-01-14 02:19:52 +0530

    SinglyLL

---
+++ SinglyLinkedList-api/Remove_Duplicates.java
@@ -0,0 +1,17 @@
+public void removeDups(LinkedList ll)
+	{
+		Node current = ll.head;
+		while(current !=null && current.next !=null)
+		{
+			if(current.value == current.next.value)
+			{
+				current.next = current.next.next;
+				ll.size--;
+			}
+			else
+			{
+				current = current.next;
+			}
+			
+		}
+	}
